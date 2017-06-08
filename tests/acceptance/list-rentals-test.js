07a1ed9084acefebb4d844f3bedae8be37ac45a5
Daniel <daniel.souza@cielo.com.br>
2017-06-08 19:50:30 -0300

    Acompanhando o tutorial até a Adding Nested Routes

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,20 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('visiting /list-rentals', function(assert) {
+  visit('/list-rentals');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/list-rentals');
+  });
+});
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
